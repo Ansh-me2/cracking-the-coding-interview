d96d7a8fade35907c585d4b6d22a46515d4c16cd
ansh-me2 <ansh.meii@gmail.com>
2018-11-17 06:50:16 +0530

    first two problems

---
+++ 1.1 duplicate string.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool unique1 (std::string & str)
+{
+  bool char_set[256];
+  std::fill(char_set, char_set + 256, false);
+  for (char &i:str)
+    {
+      int val = i;
+      if (char_set[val])
+  	       return false;
+      char_set[val] = true;
+    }
+  return true;
+}
+
+int main ()
+{
+  cout << "Enter a string ";
+  string str;
+  getline (cin, str);
+  if (unique1 (str))
+    cout << "No Duplicate";
+  else
+    cout << "Contains Duplicate";
+  return 0;
+}
